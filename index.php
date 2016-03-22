fe9ef7d1f8a972756ece72be467dd395d6795490
duhit <duhit@localhost.localdomain>
2016-03-22 15:07:25 +0000

    added static web pages hw

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Hello Cloudwick</title>
+</head>
+<body>
+<h1>Please enter your name</h1>
+<p>My Website</p>
+<form action="/list-names.php" method="GET">
+<input type="text" placeholder="type your name here" name="name">
+<button type="submit">Submit</button>
+</form>
+<div id="container">
+<php echo "Hello World"?>
+</div>
+</body>
+</html>
+
